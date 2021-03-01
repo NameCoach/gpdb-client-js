1fd740848b05dcd6265d611b0991d655cd5fc286
NameCoachDevTeam <dev-team@name-coach.com>
2021-03-01 13:52:45 +0300

    added userResponse method to pronunciations.ts; updated spec and package version

--- src/lib/repositories/pronunciations.ts
+++ src/lib/repositories/pronunciations.ts
@@ -1,21 +1,29 @@
-import IPronunciationsRepo, {
-  ComplexSearchParams,
-  SimpleSearchParams
-} from '../../types/repositories/pronunciations';
+import snakecaseKeys from 'snakecase-keys';
+
 import IHttpClient from '../../types/http-client';
 import Application from '../../types/input/application';
-import snakecaseKeys from 'snakecase-keys';
+import IPronunciationsRepo, {
+  ComplexSearchParams,
+  SimpleSearchParams,
+  UserResponseParams
+} from '../../types/repositories/pronunciations';
+
 
 export default class PronunciationsRepository implements IPronunciationsRepo {
   private httpClient: IHttpClient;
   private application: Application;
 
-  constructor(httpClient: IHttpClient, application: Application) {
+  constructor (httpClient: IHttpClient, application: Application) {
     this.httpClient = httpClient;
     this.application = application;
   }
 
-  simpleSearch({ target, targetTypeSig, targetOwnerSig, ...rest }: SimpleSearchParams): Promise<unknown> {
+  simpleSearch ({
+                  target,
+                  targetTypeSig,
+                  targetOwnerSig,
+                  ...rest
+                }: SimpleSearchParams): Promise<unknown> {
     return this.httpClient.request({
       path: '/pronunciations',
       method: 'GET',
@@ -32,7 +40,11 @@ export default class PronunciationsRepository implements IPronunciationsRepo {
     });
   }
 
-  complexSearch({ targets, userContext, ...rest }: ComplexSearchParams): Promise<unknown> {
+  complexSearch ({
+                   targets,
+                   userContext,
+                   ...rest
+                 }: ComplexSearchParams): Promise<unknown> {
     const _targets = snakecaseKeys(targets);
     const _user = snakecaseKeys(userContext);
 
@@ -49,6 +61,28 @@ export default class PronunciationsRepository implements IPronunciationsRepo {
           instance_sig: this.application.instanceSig,
           hedb_api_token: this.application.hedbApiToken
         },
+        ...rest
+      }
+    });
+  }
+
+  userResponse ({
+                  recordingId,
+                  userResponse,
+                  userContext,
+                  targetOwnerSig,
+                  ...rest
+                }: UserResponseParams): Promise<unknown> {
+    const _signature = { userContext };
+
+    return this.httpClient.request({
+      path: `/pronunciation/${recordingId}/user_response`,
+      method: 'POST',
+      contentType: 'json',
+      body: {
+        user_response: userResponse,
+        user_sig: _signature,
+        name_owner_sig: targetOwnerSig,
         ...rest
       }
     });
