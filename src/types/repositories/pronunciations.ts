fba212c7148715a6eebcb384e180081dd0f4df8a
NameCoachDevTeam <dev-team@name-coach.com>
2021-03-02 13:15:44 +0300

    added createRecording method to pronunciations.ts; updated specs and readme;

--- src/types/repositories/pronunciations.ts
+++ src/types/repositories/pronunciations.ts
@@ -1,15 +1,11 @@
+import { TargetTypeSig, UserResponse } from '../input/enum-types';
+import NameOwner from '../input/name-owner';
 import Target from '../input/tasrget';
 import User from '../input/user';
 
-export enum UserResponse {
-  Save ="save",
-  Skip = "skip",
-  Reject = "reject"
-}
-
 export interface SimpleSearchParams {
   target: string;
-  targetTypeSig: string;
+  targetTypeSig: TargetTypeSig;
   targetOwnerSig: string;
   [x: string]: any;
 }
@@ -28,8 +24,18 @@ export interface UserResponseParams {
   [x: string]: any;
 }
 
+export interface CreateRecordingParams {
+  target: string;
+  targetTypeSig: TargetTypeSig;
+  audioBase64: string;
+  userContext: User;
+  nameOwnerContext: NameOwner|{};
+  [x: string]: any;
+}
+
 export default interface IPronunciationsRepo {
   simpleSearch: (arg0: SimpleSearchParams) => Promise<unknown>;
   complexSearch: (arg0: ComplexSearchParams) => Promise<unknown>;
-  userResponse:(arg0: UserResponseParams) => Promise<unknown>
+  userResponse:(arg0: UserResponseParams) => Promise<unknown>;
+  createRecording:(arg0: CreateRecordingParams) => Promise<unknown>;
 }
