fa0e8a7b2fc39ec57f70d0ec7747e393726346d7
NameCoachDevTeam <dev-team@name-coach.com>
2021-03-02 13:15:44 +0300

    added createRecording method to pronunciations.ts; updated specs and readme;

--- src/types/input/tasrget.ts
+++ src/types/input/tasrget.ts
@@ -1,9 +1,10 @@
+import { TargetTypeSig } from './enum-types';
 import User from './user';
 
 export default interface Target {
   target: string;
   targetId?: string;
-  targetTypeSig: string;
+  targetTypeSig: TargetTypeSig;
   targetOwnerContext: User;
   pageSize?: string;
   pageNumber?: string;
