acc49c4dba4b808d38fe2cf24f8afdab748f93d4
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-21 23:01:07 +0300

    Add type imports. Update response type

---
+++ src/lib/client.ts
@@ -0,0 +1,24 @@
+import IClient from '../types/client';
+import Application from '../types/input/application';
+import IConfiguration from '../types/configuration';
+import PronunciationsRepository from './repositories/pronunciations';
+import AnalyticsEventsRepository from './repositories/analytics-events';
+import HttpClient from './http-client';
+
+export default class Client implements IClient {
+  public readonly pronunciations;
+  public readonly analyticsEvents;
+  public readonly application: Application;
+
+  constructor(application: Application, configuration: IConfiguration) {
+    this.application = application;
+
+    this.pronunciations = new PronunciationsRepository(
+      new HttpClient(configuration.apiUrl, configuration.credentials),
+      application
+    );
+    this.analyticsEvents = new AnalyticsEventsRepository(
+      new HttpClient(configuration.analyticsApiUrl, configuration.credentials),
+    );
+  }
+}
