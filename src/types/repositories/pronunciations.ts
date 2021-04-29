43ec55986d75b2927d7330d457562e92947974e1
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-29 20:10:12 +0300

    Add [POST] recording request endpoint

--- src/types/repositories/pronunciations.ts
+++ src/types/repositories/pronunciations.ts
@@ -33,9 +33,18 @@ export interface CreateRecordingParams {
   [x: string]: any;
 }
 
+export interface CreateRecordingRequestParams {
+  target: string;
+  targetTypeSig: TargetTypeSig;
+  targetOwnerContext: User;
+  userContext: User;
+  [x: string]: any;
+}
+
 export default interface IPronunciationsRepo {
   simpleSearch: (arg0: SimpleSearchParams) => Promise<any>;
   complexSearch: (arg0: ComplexSearchParams) => Promise<any>;
   userResponse:(arg0: UserResponseParams) => Promise<any>;
   createRecording:(arg0: CreateRecordingParams) => Promise<any>;
+  createRecordingRequest: (arg0: CreateRecordingRequestParams) => Promise<any>;
 }
