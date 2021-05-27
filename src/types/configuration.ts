b29c39a9433d56f5b798cbf981b21f388c243328
NameCoachDevTeam <dev-team@name-coach.com>
2021-05-27 17:01:37 +0300

    [#178070748] Add headers to configuration
    https://www.pivotaltracker.com/n/projects/2183482/stories/178070748

--- src/types/configuration.ts
+++ src/types/configuration.ts
@@ -1,9 +1,12 @@
 import ICredentials from './credentials';
 
+export type HeadersInit = Headers | string[][] | { [key: string]: string };
+
 export default interface IConfiguration {
   credentials: ICredentials;
   accessKeyId: string;
   secretAccessKey: string;
   apiUrl: string;
   analyticsApiUrl?: string;
+  headers: HeadersInit;
 }
