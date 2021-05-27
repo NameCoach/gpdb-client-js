88b061f4a5d95ffe3f3101e99fbd698f5e6116b8
NameCoachDevTeam <dev-team@name-coach.com>
2021-05-27 17:01:37 +0300

    [#178070748] Add headers to configuration
    https://www.pivotaltracker.com/n/projects/2183482/stories/178070748

---
+++ src/lib/configuration.ts
@@ -0,0 +1,41 @@
+import IConfiguration, { HeadersInit } from '../types/configuration';
+import ICredentials from '../types/credentials';
+
+import Credentials from './credentials';
+
+const env = process === undefined ? {} : process.env;
+
+export default class Configuration implements IConfiguration {
+  public readonly credentials: ICredentials;
+  accessKeyId: string;
+  apiUrl: string;
+  secretAccessKey: string;
+  analyticsApiUrl: string;
+  headers: HeadersInit;
+
+  constructor({
+      accessKeyId = env.GPDB_ACCESS_KEY_ID,
+      secretAccessKey = env.GPDB_SECRET_ACCESS_KEY,
+      apiUrl = env.GPDB_API_URL,
+      analyticsApiUrl = env.GPDB_ANALYTICS_API_URL,
+      headers = {},
+    }) {
+
+    if (!accessKeyId)
+      throw Error("GPDB access key id must be defined");
+
+    if (!secretAccessKey)
+      throw Error("GPDB secret access key must be defined");
+
+    if (!apiUrl)
+      throw Error("GPDB api URL must be defined");
+
+    this.accessKeyId = accessKeyId;
+    this.secretAccessKey = secretAccessKey;
+    this.apiUrl = apiUrl;
+    this.analyticsApiUrl = analyticsApiUrl;
+    this.headers = headers;
+
+    this.credentials = new Credentials(accessKeyId, secretAccessKey)
+  }
+}
