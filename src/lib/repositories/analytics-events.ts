088b035ba68f44cb4fb6f04ea7e4831cad4c0955
NameCoachDevTeam <dev-team@name-coach.com>
2021-05-28 12:55:28 +0300

    Add gitlab CI config

---
+++ src/lib/repositories/analytics-events.ts
@@ -0,0 +1,23 @@
+import IAnalyticsEventsRepo from '../../types/repositories/analytics-events';
+import IHttpClient from '../../types/http-client';
+import AnalyticEvent from '../../types/input/analytic-event';
+import snakecaseKeys from 'snakecase-keys';
+
+export default class AnalyticsEventsRepository implements IAnalyticsEventsRepo {
+  private httpClient: IHttpClient;
+
+  constructor(httpClient: IHttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  create(event: AnalyticEvent): Promise<any> {
+    const _event = snakecaseKeys(event);
+
+    return this.httpClient.request({
+      path: '/analytics_events',
+      method: 'POST',
+      contentType: 'json',
+      body: _event
+    });
+  }
+}
