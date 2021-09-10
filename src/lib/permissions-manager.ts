fc1225913f94072b071a4273667f5819050a83d7
NameCoachDevTeam <dev-team@name-coach.com>
2021-09-10 16:07:48 +0300

    NAM-274 update permissions-manager and fix exclude actions support

--- src/lib/permissions-manager.ts
+++ src/lib/permissions-manager.ts
@@ -8,13 +8,23 @@ export default class PermissionsManager implements IPermissionsManager{
     this.permissions = permissions;
   }
 
-  can(resource: Resources, permission: string): boolean {
+  can(resource: Resources, permission: string): boolean;
+  can(resource: Resources): boolean;
+
+  can(resource: Resources, permission?: string): boolean {
     const _resource = this.permissions[resource];
+
     if (!_resource) return false;
 
-    if (_resource.includes('*'))
+    const _permission = permission;
+
+    if(!_permission) return true;
+
+    if(_resource.exclude_actions.includes(_permission)) return false;
+
+    if (_resource.actions.includes('*'))
       return true;
 
-    return _resource.includes(permission);
+    return _resource.actions.includes(_permission);
   }
 }
