0bd2c8500172b9899a320d49686506d4e891a3c6
NameCoachDevTeam <dev-team@name-coach.com>
2022-02-16 16:31:40 +0300

    NAM-603 added custom-attributes repository

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,11 +1,13 @@
 import Application from './input/application';
 import AnalyticsEventsRepo from './repositories/analytics-events';
 import BrowserExtensionRepo from './repositories/browser-extension';
+import CustomAttributesRepo from './repositories/custom-attributes';
 import PronunciationsRepo from './repositories/pronunciations';
 
 export default interface IClient {
   application: Application,
   pronunciations: PronunciationsRepo;
   analyticsEvents: AnalyticsEventsRepo;
-  browserExtension: BrowserExtensionRepo
+  browserExtension: BrowserExtensionRepo;
+  customAttributes: CustomAttributesRepo;
 }
