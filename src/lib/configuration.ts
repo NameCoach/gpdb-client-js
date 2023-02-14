5198662b65f4dea92142406c7d7de2d05884e271
NameCoachDevTeam <dev-team@name-coach.com>
2023-02-14 18:19:12 +0300

    Resolve invalid env imports

--- src/lib/configuration.ts
+++ src/lib/configuration.ts
@@ -3,8 +3,6 @@ import ICredentials from '../types/credentials';
 
 import Credentials from './credentials';
 
-const env = process === undefined ? {} : process.env;
-
 export default class Configuration implements IConfiguration {
   public readonly credentials: ICredentials;
   accessKeyId: string;
@@ -14,21 +12,17 @@ export default class Configuration implements IConfiguration {
   headers: HeadersInit;
 
   constructor({
-      accessKeyId = env.GPDB_ACCESS_KEY_ID,
-      secretAccessKey = env.GPDB_SECRET_ACCESS_KEY,
-      apiUrl = env.GPDB_API_URL,
-      analyticsApiUrl = env.GPDB_ANALYTICS_API_URL,
-      headers = {},
-    }) {
+    accessKeyId = process?.env?.GPDB_ACCESS_KEY_ID,
+    secretAccessKey = process?.env?.GPDB_SECRET_ACCESS_KEY,
+    apiUrl = process?.env?.GPDB_API_URL,
+    analyticsApiUrl = process?.env?.GPDB_ANALYTICS_API_URL,
+    headers = {},
+  }) {
+    if (!accessKeyId) throw Error('GPDB access key id must be defined');
 
-    if (!accessKeyId)
-      throw Error("GPDB access key id must be defined");
+    if (!secretAccessKey) throw Error('GPDB secret access key must be defined');
 
-    if (!secretAccessKey)
-      throw Error("GPDB secret access key must be defined");
-
-    if (!apiUrl)
-      throw Error("GPDB api URL must be defined");
+    if (!apiUrl) throw Error('GPDB api URL must be defined');
 
     this.accessKeyId = accessKeyId;
     this.secretAccessKey = secretAccessKey;
@@ -36,6 +30,6 @@ export default class Configuration implements IConfiguration {
     this.analyticsApiUrl = analyticsApiUrl;
     this.headers = headers;
 
-    this.credentials = new Credentials(accessKeyId, secretAccessKey)
+    this.credentials = new Credentials(accessKeyId, secretAccessKey);
   }
 }
